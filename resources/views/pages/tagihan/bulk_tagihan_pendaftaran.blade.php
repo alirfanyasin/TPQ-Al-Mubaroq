32c3a90956031c83e7ce4b33ba2ec38d39689003
alirfanyasin <risayunita21@gmail.com>
2025-02-13 16:22:42 +0700

    feat: create feature bulk payment monthly

--- resources/views/pages/tagihan/bulk_tagihan_pendaftaran.blade.php
+++ resources/views/pages/tagihan/bulk_tagihan_pendaftaran.blade.php
@@ -2,13 +2,13 @@
 @section('breadcrumb')
   <div class="row">
     <div class="order-last col-12 col-md-6 order-md-1">
-      <h3>Tagihan Santri</h3>
+      <h3>Bulk Tagihan Pendaftaran</h3>
     </div>
     <div class="order-first col-12 col-md-6 order-md-2">
       <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
         <ol class="breadcrumb">
           <li class="breadcrumb-item"><a href="index.html">App</a></li>
-          <li class="breadcrumb-item active" aria-current="page">Tagihan Santri</li>
+          <li class="breadcrumb-item active" aria-current="page">Bulk Tagihan Pendaftaran</li>
         </ol>
       </nav>
     </div>
@@ -21,17 +21,6 @@
         <div class="card-header">
           <a href="{{ route('santri.history_tagihan_bulanan') }}" class="btn btn-primary icon icon-left">Riwayat Tagihan
             Bulanan</a>
-
-          <div class="d-inline-block">
-            <div class="input-group">
-              <button class="btn btn-success" type="button" id="button-bulk" onclick="handleBulkAction()">Bulk</button>
-              <select class="form-select" id="bulk-action-select" aria-label="Pilih tindakan bulk">
-                <option value="{{ route('tagihan.bulk.tagihan_pendaftaran') }}">Tagihan Pendaftaran</option>
-                <option value="{{ route('tagihan.bulk.tagihan_seragam') }}">Tagihan Seragam</option>
-                <option value="{{ route('tagihan.bulk.tagihan_bulanan') }}">Tagihan Bulanan</option>
-              </select>
-            </div>
-          </div>
         </div>
         <div class="card-body">
           <table class="table table-striped" id="table1">
@@ -41,8 +30,6 @@
                 <th>Nama Lengkap</th>
                 <th>Jenis Kelamin</th>
                 <th>Tagihan Pendaftaran</th>
-                <th>Tagihan Seragam</th>
-                <th>Tagihan Bulanan</th>
                 <th>Menu</th>
               </tr>
             </thead>
@@ -56,12 +43,6 @@
                   <td>{{ $no++ }}</td>
                   <td>{{ $data->nama_lengkap }}</td>
                   <td>{{ $data->jenis_kelamin }}</td>
-                  <td><span
-                      class="badge {{ $data->tagihanPendaftaran->status == 'Belum Lunas' ? 'bg-danger' : 'bg-success' }}">{{ $data->tagihanPendaftaran->status }}</span>
-                  </td>
-                  <td><span
-                      class="badge {{ $data->tagihanSeragam->status == 'Belum Lunas' ? 'bg-danger' : 'bg-success' }}">{{ $data->tagihanSeragam->status }}</span>
-                  </td>
                   <td>
                     @if ($data->tagihanBulanIni)
                       <span
@@ -99,16 +80,4 @@
 @push('js')
   <script src="/template/assets/extensions/simple-datatables/umd/simple-datatables.js"></script>
   <script src="/template/assets/js/pages/simple-datatables.js"></script>
-
-
-  <script>
-    function handleBulkAction() {
-      const selectedValue = document.getElementById('bulk-action-select').value;
-      if (selectedValue) {
-        window.location.href = selectedValue;
-      } else {
-        alert('Pilih tindakan terlebih dahulu.');
-      }
-    }
-  </script>
 @endpush
